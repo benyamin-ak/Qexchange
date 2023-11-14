89a11282d164ea1a16862115d9905aa099ad4671
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2023-11-14 12:17:47 +0330

    feat: orderContract interface defined

--- contracts/order_contract.go
+++ contracts/order_contract.go
@@ -1,3 +1,9 @@
 package contracts
 
-//TODO: implement
+type OrderContract interface {
+	//buy(coinID int, amount float64) error
+	buy(int, float64) error
+	//sell(coinID int, amount float64) error
+	sell(int, float64) error
+	cancel() error
+}
