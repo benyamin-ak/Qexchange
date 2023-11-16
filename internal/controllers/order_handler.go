4210f4f451e63d6f4c90f872377969f603c36f39
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2023-11-16 12:03:45 +0330

    feat: order handler added

---
+++ internal/controllers/order_handler.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"Qexchange/internal/core/contracts"
+	"Qexchange/internal/core/services"
+
+	echo "github.com/labstack/echo/v4"
+)
+
+type OrderRequest struct {
+	UserID int     `json:"user_id"`
+	CoinID int     `json:"coin_id"`
+	Amount float64 `json:"amount"`
+}
+
+type OrderHandler struct {
+	osc contracts.OrderCoreContract
+}
+
+func NewOrderHandler() *OrderHandler {
+	return &OrderHandler{
+		osc: services.NewOrderService(),
+	}
+}
+
+func DefineRoutes(e *echo.Echo) {
+	oh := NewOrderHandler()
+	e.POST("/buy", oh.Buy)
+	e.POST("/sell", oh.Sell)
+	e.POST("/cancel", oh.Cancel)
+}
+
+func (oh *OrderHandler) Buy(c echo.Context) error {
+	return nil
+}
+
+func (oh *OrderHandler) Sell(c echo.Context) error {
+	return nil
+}
+
+func (oh *OrderHandler) Cancel(c echo.Context) error {
+	return nil
+}
--- internal/core/services/order_db_service.go
+++ internal/core/services/order_db_service.go
@@ -1,6 +1,7 @@
 package services
 
 type OrderDBService struct {
+	//infrastructure.OrderDBRepository
 }
 
 func NewOrderDBService() *OrderDBService {
