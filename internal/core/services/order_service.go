2de707d0c6d5d4de07153d569b9d94a2d80a1163
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2023-11-16 14:25:16 +0330

    feat: order service buy implemented

--- internal/core/services/order_service.go
+++ internal/core/services/order_service.go
@@ -1,6 +1,12 @@
 package services
 
-import "Qexchange/internal/core/contracts"
+import (
+	"Qexchange/internal/core/contracts"
+	"Qexchange/internal/core/models"
+	"errors"
+	"math"
+	"time"
+)
 
 type OrderService struct {
 	dbc contracts.OrderDBContract
@@ -13,7 +19,29 @@ func NewOrderService() *OrderService {
 }
 
 func (os *OrderService) Buy(userID int, coinID int, amount float64) (int, error) {
-	return 0, nil
+	o := models.Order{
+		UserID:    userID,
+		Side:      "buy",
+		CoinID:    coinID,
+		Quantity:  amount,
+		Timestamp: time.Now(),
+		Status:    models.OrderStatusActive,
+	}
+	if err := os.validateData(o); err != nil {
+		return math.MinInt, err
+	}
+	balance := os.dbc.GetUserBalance(userID)
+	price := os.dbc.GetCoinPrice(coinID)
+	commission := os.dbc.GetCoinCommission(coinID)
+	_ = commission
+	if balance < amount*price {
+		return math.MinInt, errors.New("insufficient funds")
+	}
+	ID, err := os.dbc.CommitOrder(o)
+	if err != nil {
+		return math.MinInt, err
+	}
+	return ID, nil
 }
 
 func (os *OrderService) Sell(userID int, coinID int, amount float64) (int, error) {
@@ -21,5 +49,15 @@ func (os *OrderService) Sell(userID int, coinID int, amount float64) (int, error
 }
 
 func (os *OrderService) Cancel(userID int, orderID int, userPassword string) error {
+	return nil
+}
+
+func (os *OrderService) validateData(o models.Order) error {
+	if err := os.dbc.ValidateUser(o.UserID); err != nil {
+		return err
+	}
+	if err := os.dbc.ValidateCoin(o.CoinID); err != nil {
+		return err
+	}
 	return nil
 }
