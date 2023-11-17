acdffe82e51c47e8973f4eec75b8f04d5e1a1745
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2023-11-17 21:13:34 +0330

    refactor: OrderDBContract Submit function's return types deleted

--- internal/core/contracts/order_contract.go
+++ internal/core/contracts/order_contract.go
@@ -3,11 +3,8 @@ package contracts
 import "Qexchange/internal/core/models"
 
 type OrderCoreContract interface {
-	//buy(userID int, coinID int, amount float64) orderID error
 	Buy(int, int, float64) (int, error)
-	//sell(userID int, coinID int, amount float64) orderID error
 	Sell(int, int, float64) (int, error)
-	//cancel(userID int, orderID int, userPassword string) error
 	Cancel(int, int, string) error
 }
 
@@ -16,7 +13,7 @@ type OrderDBContract interface {
 	GetCoinPrice(int) (float64, error)
 	GetCoinCommission(int) (float64, error)
 	CreateOrder(models.Order) (int, error)
-	SubmitOrder(models.Order) (int, error)
+	SubmitOrder(models.Order)
 	ChangeOrderStatus(models.Order, int) error
 	ValidateUserPassword(int, string) error
 }
