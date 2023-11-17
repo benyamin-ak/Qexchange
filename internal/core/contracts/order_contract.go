dda59cc57eb9b7696d650473e9d18d2475203bff
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2023-11-17 20:58:13 +0330

    refactor: OrderDBContract changed due to separate order creation and submission

--- internal/core/contracts/order_contract.go
+++ internal/core/contracts/order_contract.go
@@ -15,7 +15,8 @@ type OrderDBContract interface {
 	GetUserBalance(int, int) (float64, error)
 	GetCoinPrice(int) (float64, error)
 	GetCoinCommission(int) (float64, error)
-	CommitOrder(models.Order) (int, error)
+	CreateOrder(models.Order) (int, error)
+	SubmitOrder(models.Order) (int, error)
 	ChangeOrderStatus(models.Order, int) error
 	ValidateUserPassword(int, string) error
 }
